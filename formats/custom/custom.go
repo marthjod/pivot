059e6f79ef02e1b6e3813642a5ff41eef4dbbb15
marthjod@gmail.com <marthjod@gmail.com>
2018-08-07 18:14:47 +0200

    un-export renderer helpers

--- formats/custom/custom.go
+++ formats/custom/custom.go
@@ -2,34 +2,30 @@ package custom
 
 import (
 	"fmt"
-	"github.com/marthjod/pivot/model"
-	"github.com/marthjod/pivot/convert"
-	"gopkg.in/yaml.v2"
-	"strings"
 	"sort"
+	"strings"
+
+	"github.com/marthjod/pivot/convert"
+	"github.com/marthjod/pivot/model"
+	"gopkg.in/yaml.v2"
 )
 
+// Service represents a renderable service definition.
 type Service map[string]map[string]interface{}
 
-func (s Service) Yaml() (string, error) {
-	y, err := yaml.Marshal(s)
-	if err != nil {
-		return "", err
-	}
-	return string(y), nil
-}
-
+// ServiceConverter knows how to convert a service definition.
 type ServiceConverter struct {
 	convert.Converter
-	Pivio *model.Pivio
+	Pivio   *model.Pivio
 	Aliases *convert.Aliases
 }
 
+// Render implements the Converter interface.
 func (c *ServiceConverter) Render() (string, error) {
-	return c.Convert().Yaml()
+	return c.convert().toYAML()
 }
 
-func (c *ServiceConverter) Convert() Service {
+func (c *ServiceConverter) convert() Service {
 	zones := []string{
 		fmt.Sprintf("zone-%s-%s", strings.ToUpper(c.Pivio.Runtime.NetworkZone), strings.ToUpper(c.Pivio.ShortName)),
 		"zone-LOGGING",
@@ -40,11 +36,19 @@ func (c *ServiceConverter) Convert() Service {
 
 	return map[string]map[string]interface{}{
 		fmt.Sprintf("service-%s", c.Pivio.ShortName): {
-			"cpu": c.Aliases.Get("cpu", c.Pivio.Runtime.CPU),
-			"vcpu": c.Aliases.Get("vcpu", c.Pivio.Runtime.CPU),
-			"image": c.Aliases.Get("image", c.Pivio.Runtime.Disk),
-			"memory": c.Aliases.Get("memory", c.Pivio.Runtime.RAM),
+			"cpu":      c.Aliases.Get("cpu", c.Pivio.Runtime.CPU),
+			"vcpu":     c.Aliases.Get("vcpu", c.Pivio.Runtime.CPU),
+			"image":    c.Aliases.Get("image", c.Pivio.Runtime.Disk),
+			"memory":   c.Aliases.Get("memory", c.Pivio.Runtime.RAM),
 			"networks": zones,
 		},
 	}
+}
+
+func (s Service) toYAML() (string, error) {
+	y, err := yaml.Marshal(s)
+	if err != nil {
+		return "", err
+	}
+	return string(y), nil
 }
--- formats/simple/simple.go
+++ formats/simple/simple.go
@@ -1,37 +1,41 @@
 package simple
 
 import (
-	"github.com/marthjod/pivot/model"
-	"github.com/marthjod/pivot/convert"
 	"encoding/json"
+
+	"github.com/marthjod/pivot/convert"
+	"github.com/marthjod/pivot/model"
 )
 
+// Simple represents a simple renderable service definition.
 type Simple map[string]map[string]interface{}
 
-func (d Simple) Json() (string, error) {
-	y, err := json.MarshalIndent(d, "", "  ")
-	if err != nil {
-		return "", err
-	}
-	return string(y), nil
-}
-
+// Converter is a converter for simple service definitions.
 type Converter struct {
 	convert.Converter
 	Pivio *model.Pivio
 }
 
+// Render implements the converter interface.
 func (d Converter) Render() (string, error) {
-	return d.Convert().Json()
+	return d.convert().toJSON()
 }
 
-func (d Converter) Convert() Simple {
+func (d Converter) convert() Simple {
 	return map[string]map[string]interface{}{
 		d.Pivio.ShortName: {
-			"cpu": d.Pivio.Runtime.CPU,
-			"disk": d.Pivio.Runtime.Disk,
+			"cpu":    d.Pivio.Runtime.CPU,
+			"disk":   d.Pivio.Runtime.Disk,
 			"memory": d.Pivio.Runtime.RAM,
-			"zone": d.Pivio.Runtime.NetworkZone,
+			"zone":   d.Pivio.Runtime.NetworkZone,
 		},
 	}
+}
+
+func (d Simple) toJSON() (string, error) {
+	y, err := json.MarshalIndent(d, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(y), nil
 }
